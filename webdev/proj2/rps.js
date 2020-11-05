f9dec56bee73d29ec5fe8d369299d072d164b706
Nick Perrone <njperrone@gmail.com>
2020-11-05 17:26:55 -0500

    finished rock paper scissors

--- webdev/proj2/rps.js
+++ webdev/proj2/rps.js
@@ -10,36 +10,43 @@ const WINNERS = {
     TIE : 2
 }
 
-function theGame(){
+function playGame(){
 // main function
     // initialize tracking
     let results = [];
     let winner;
     let rounds;
+    let names = ["Rock", "Paper", "Scissors"];
+    let trackArr;
     
     //prompt rounds
     rounds = prompt("Welcome to Rock, Paper, Scissors! How many rounds do you want to play?");
-    debugger;
+    // debugger;
     // get pick
     for (let i = 0; i < rounds; i++){
+
+    trackArr = winnerTracking(results);
+    alert(`Round ${i+1}. Wins: ${trackArr[1]}. Losses: ${trackArr[0]}. Ties: ${trackArr[2]}.`);
     let computerPick = randomNumber3();
     let playerPick = getPick();
-
+    // debugger;
     // determine and track WINNERS
+    alert(`The computer chose ${names[computerPick]}`);
     winner = rockPaperScissors(computerPick, playerPick);
     results.push(winner);
     }
 
     // report winner and history
-    winnerTracking(results);
+    trackArr = winnerTracking(results);
+    alert(`The game is over! You won ${trackArr[1]} times. You lost ${trackArr[0]} times. You tied with the computer ${trackArr[2]} times.`);
     alert("Thank you for playing.");
 }
 
 //helpers
 function rockPaperScissors(computerPick, playerPick){
-    //RPS game here
+    // RPS game here
     let winner;
-    debugger;
+    // debugger;
     if (computerPick === playerPick){
         winner = WINNERS.TIE;
         alert("It was a tie, sorry.");
@@ -67,35 +74,35 @@ function rockPaperScissors(computerPick, playerPick){
         }
     } else if (computerPick === RPS.SCISSORS){
         switch (playerPick){
-            case RPS.PAPER:
+            case RPS.ROCK:
                 winner = WINNERS.PLAYER;
                 alert("You won!");
                 break;
-            case RPS.ROCK:
+            case RPS.PAPER:
                 winner = WINNERS.COMPUTER;
                 alert("You lost. Tough luck.");
                 break;
         }
     }
 
-    debugger;
+    // debugger;
     return winner;
 }
 
 function randomNumber3(){
-    debugger;
+    // debugger;
     let randomNum = Math.random();
 
     randomNum = Math.floor(randomNum*3);
 
-    debugger;
+    // debugger;
     return randomNum;
 }
 
 function getPick(){
-    let pick = prompt("Rock, Paper, or Scissors?");
+    let pick = prompt(`Rock, Paper, or Scissors?`);
     let pickNum;
-    debugger;
+    // debugger;
     pick = pick.toLowerCase();
 
     switch (pick){
@@ -112,19 +119,20 @@ function getPick(){
             pickNum = RPS.ROCK;
             break;
     }
-    debugger;
+    // debugger;
     return pickNum;
 }
 
 function winnerTracking(winArr){
     // use a tracking array
-    let tracker = new Array(3);
-    debugger;
+    let trackArr = [0,0,0];
     // i corresponds to WINNERS value
     // tracker[i] is number of wins for each i.
-    winArr.forEach(element => {
-        tracker[element] += 1;
-    });
-    debugger;
-    alert(`The game is over! You won ${tacker[1]} times. You lost ${tacker[0]} times. You tied with the computer ${tacker[2]} times.`);
+    if (winArr.length > 0){
+        winArr.forEach(element => {
+            trackArr[element] += 1;
+        });
+    }
+
+    return trackArr;
 }
