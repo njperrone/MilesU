671384c892dedf1192f09c47196043d47a8d2f2e
Nick Perrone <njperrone@gmail.com>
2020-11-06 00:57:01 -0500

    calculator skelleton made

---
+++ webdev/calculator/scripts.js
@@ -0,0 +1,39 @@
+//Enums and Globals
+
+function main(){
+    // Flow control
+
+    // get form data
+
+    //verify inputs
+
+    // choose the right function
+
+    //adjust the reports.
+}
+
+function addNums(a,b){
+    // Adds numbers
+}
+
+function subtractNums(a,b){
+    // Subtracts numbers
+}
+
+function multiply(a,b){
+    //Multiplies numbers
+}
+
+function divide(a,b){
+    //Divides numbers
+    if (b === 0){
+        // probably going to move this logic elsewhere
+        return alert("You cannot divide by ZERO.")
+    } else{
+        return a/b;
+    }
+}
+
+function clearForm(){
+    //Clears forms and resets radio
+}
