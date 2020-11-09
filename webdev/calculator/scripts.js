9a525cc0486f25167ca0fd7cc5fdf0646fca4ffd
Nick Perrone <njperrone@gmail.com>
2020-11-08 22:31:56 -0500

    calculator finished

--- webdev/calculator/scripts.js
+++ webdev/calculator/scripts.js
@@ -1,5 +1,5 @@
 //Enums and Globals
-
+const originalMessage = document.getElementById("results").innerHTML;
 function main(){
     // Flow control
 
@@ -7,65 +7,74 @@ function main(){
     let num1 = document.getElementById("number-1").value;
     let num2 = document.getElementById("number-2").value;
     let operation = document.querySelector('input[name="operation"]:checked').value;
-    let result;
-    let opSymbol;
+    
 
     // console.log(`number 1: ${num1}. number 2: ${num2}`);
     // console.log(operation);
-
+debugger;
     //verify inputs
-
-    // choose the right function
-    switch (operation){
-        case "add":
-            result = addNums(num1, num2);
-            opSymbol = "+";
-            break;
-        case "subtract":
-            result = subtractNums(num1, num2);
-            opSymbol = "-";
-            break;
-        case "multiply":
-            result = multiply(num1, num2);
-            opSymbol = "*";
-            break;
-        case "divide":
-            result = divide(num1, num2);
-            opSymbol = "/"
-            break;
+    if (num1 === "" || num2 === ""){
+        document.getElementById("results").innerHTML = "Please enter valid numbers."
+    } 
+    else{
+        
+        // choose the right function
+        switch (operation){
+            case "add":
+                addNums(num1, num2);
+                break;
+            case "subtract":
+                subtractNums(num1, num2);
+                break;
+            case "multiply":
+                multiply(num1, num2);
+                break;
+            case "divide":
+                divide(num1, num2);
+                break;
+        }
+                        
     }
-
-    //adjust the reports.
-    document.getElementById("results").innerHTML = `${num1} ${opSymbol} ${num2} = ${result}`;
+    //adjust the reports.    
 }
-
+                    
 function clearForm(){
     //Clears forms and resets radio
     document.getElementById("calculator").reset();
-    document.getElementById("number-1").focus();
+    document.getElementById("first-num").focus();
+    document.getElementById("results").innerHTML = originalMessage;
 }
 
 function addNums(a,b){
-    return Number(a) + Number(b);
+    let x = Number(a) + Number(b);
+
+    document.getElementById("results").innerHTML = `${b} + ${a} = ${x}`;
+
     // Adds numbers
 }
 
 function subtractNums(a,b){
     // Subtracts numbers
-    return a-b;
+    let x = a-b;
+
+    document.getElementById("results").innerHTML = `${b} - ${a} = ${x}`;
 }
 
 function multiply(a,b){
     //Multiplies numbers
-    return a*b;
+    let x = a*b;
+
+    document.getElementById("results").innerHTML = `${b} * ${a} = ${x}`;
 }
 
 function divide(a,b){
     //Divides numbers
     if (b == 0){
         // probably going to move this logic elsewhere
-        return "You cannot divide by ZERO."
+        document.getElementById("results").innerHTML = `You cannot divide by ZERO!`;
     } else{
-        return a/b;
+        let x = a/b;
+
+        document.getElementById("results").innerHTML = `${a} / ${b} = ${x}`;
     }
 }
